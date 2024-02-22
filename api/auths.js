fa06885ce9168eaf6ccbfb98d92b60338e553675
unknown <you@example.com>
2024-02-21 23:27:54 -0500

    validacion de votos

--- api/auths.js
+++ api/auths.js
@@ -7,3 +7,17 @@ const API = "https://votacionessena.up.railway.app";
 
 // Realiza una solicitud de inicio de sesión al servidor back-end.
 export const loginRequest = (values) => axios.post(`${API}/api/v2/login`, values)
+
+export const getCandidatos = (token) => axios.get(`${API}/api/v2/candidatos`, {
+    headers: {
+        Authorization: `Bearer ${token}`,
+    },
+});
+
+
+export const voto = (token, values) =>
+    axios.post(`${API}/api/v2/votos`, { candidatoID: values }, {
+        headers: {
+            Authorization: `Bearer ${token}`,
+        },
+    });
